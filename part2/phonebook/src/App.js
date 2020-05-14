0086dad51462c6c2ea5d41c50608bfb68ea02731
Mauro Brandão <maurobrandao.job@gmail.com>
2020-05-14 18:29:31 +0100

    2.9: The Phonebook Step4
    exercise 2.9 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,7 +6,10 @@ const Button =(props) =>(
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { id: 0, name: 'Arto Hellas', number: '' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const setToNewName = () => setNewName(document.getElementById("name").value)
@@ -20,6 +23,18 @@ const App = () => {
   const handleNumberChange = (event) => {
     setToNewNumber(event.target.value);
   };
+
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const [searchResults, setSearchResults] = React.useState([]);
+  const handleChange = event => {
+      setSearchTerm(event.target.value);
+    };
+ React.useEffect(() => {
+    const results = persons.filter(person =>
+      person.name.includes(searchTerm)
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -37,20 +52,35 @@ const App = () => {
         number: newNumber,
         id: persons.length + 1,
       };
-      setPersons(persons.concat(personObject));
+      const newpersons = persons.concat(personObject)
+      setPersons(newpersons);
+      setSearchResults(newpersons);
       setNewName('');
       setNewNumber('');
+      setSearchTerm('');
+
     }
     
   }
-  function PersonsList() {
-    const listItems = persons.map(person => <div key={person.id}>{person.name} {person.number}</div>);  return (
-      <div>{listItems}</div>  );
+  /*function PersonsList() {
+    const listItems = persons.map(person => <div key={person.id}>{person.name} {person.number}</div>);  
+      return ( <div>{listItems}</div>  );
+  }*/
+
+  function PersonsFilteredList() {
+    const listItems = searchResults.map(person => <div key={person.id}>{person.name} {person.number}</div>);  
+      return ( <div>{listItems}</div>  );
   }
 
   return (
     <div style={{margin:'4px'}}>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with<input type="text" placeholder="Search"
+        value={searchTerm}
+        onChange={handleChange}></input>
+      </div>
+      <h3>add a new</h3>
       <form onSubmit={addPerson}>
         <div>
           name: <input id="name" type="text" value={newName} onChange={handleNameChange} />
@@ -63,7 +93,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <PersonsList/>
+      <PersonsFilteredList />
     </div>
   )
 }
