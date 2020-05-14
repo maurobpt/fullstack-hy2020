5d391c63dadbc7014ac08b56ffdb5835885ee71b
Mauro Brandão <maurobrandao.job@gmail.com>
2020-05-14 17:07:37 +0100

    2.8: The Phonebook Step3
    exercise 2.8 complete

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,14 +6,19 @@ const Button =(props) =>(
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { id: 0, name: 'Arto Hellas' }
+    { id: 0, name: 'Arto Hellas', number: '' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const setToNewName = () => setNewName(document.getElementById("name").value)
+  const [ newNumber, setNewNumber ] = useState('')
+  const setToNewNumber = () => setNewNumber(document.getElementById("number").value)
   
   const handleNameChange = (event) => {
     setToNewName(event.target.value);
-    //console.log(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setToNewNumber(event.target.value);
   };
 
   const addPerson = (event) => {
@@ -29,15 +34,17 @@ const App = () => {
     if(readyToAdd==true){
       const personObject = {
         name: newName,
+        number: newNumber,
         id: persons.length + 1,
       };
       setPersons(persons.concat(personObject));
       setNewName('');
+      setNewNumber('');
     }
     
   }
   function PersonsList() {
-    const listItems = persons.map(person => <div key={person.id}>{person.name}</div>);  return (
+    const listItems = persons.map(person => <div key={person.id}>{person.name} {person.number}</div>);  return (
       <div>{listItems}</div>  );
   }
 
@@ -47,6 +54,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input id="name" type="text" value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input id="number" type="tel" pattern="[0-9]{3}-[0-9]{6}" value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
         <Button text="add" />
