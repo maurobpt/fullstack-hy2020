5a2f652ca1df77bdf364f6ac351baa003d410276
Mauro Brandão <maurobrandao.job@gmail.com>
2020-06-10 17:48:10 +0100

    4.22: bloglist expansion, step10
    exercise 4.22 complete

--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -8,6 +8,16 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog.toJSON())
+  } else {
+    response.status(404).end()
+  }
+})
+
+
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body
 
