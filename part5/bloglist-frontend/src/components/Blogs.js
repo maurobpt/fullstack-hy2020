4b4ba5a89d38b12a7ab23856ffa6435f1a2fc28d
Mauro Brandão <maurobrandao.job@gmail.com>
2020-06-11 15:49:29 +0100

    5.8: Blog list frontend, step8
    exercise 5.8 complete

--- part5/bloglist-frontend/src/components/Blogs.js
+++ part5/bloglist-frontend/src/components/Blogs.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Blog from './Blog'
 
-const Blogs = ({ blogs, handleLikes }) => {
+const Blogs = ({ blogs, users, handleLikes }) => {
   return (
     <div>
-      {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} handleLikes={handleLikes} />
-      )}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog => {
+          const user = users.find(user => blog.user.id ? user.id === blog.user.id  : user.id === blog.user )
+          return(
+            <Blog
+              key={blog.id}
+              blog={blog}
+              user={user}
+              handleLikes={handleLikes}
+            />
+          )
+        })
+      }
     </div>
   )
 }
